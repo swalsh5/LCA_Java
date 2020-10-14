dacd965c7dc2df5c2751aadc6348f8bd1df8951d
Sam Walsh <sam.walsh31@gmail.com>
2020-10-14 21:07:57 +0100

    See if I can get this working

---
+++ LCA_Sweng/Node.java
@@ -0,0 +1,13 @@
+/* Class containing left and right child of current 
+node and key value*/
+class Node 
+{ 
+	int data; 
+	Node left, right; 
+
+	public Node(int item) 
+	{ 
+		data = item; 
+		left = right = null; 
+	} 
+} 
